ce31f60803a3ce154378ee3b4a913dbde775571a
andreseichi <andre_seichi@hotmail.com>
2022-05-12 00:45:15 -0300

    impedindo de virar a pergunta novamente

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -12,16 +12,23 @@ import './styles.css';
 
 export function Card({ question, answer, index }) {
   const [isQuestionShowing, setIsQuestionShowing] = useState(false);
+  const [isAnswered, setIsAnswered] = useState(false);
   const [color, setColor] = useState('');
   const [icon, setIcon] = useState(Icon);
 
   function toggleQuestionShowing(colorProp) {
-    setIsQuestionShowing(!isQuestionShowing);
+    setIsQuestionShowing(false);
+    setIsAnswered(true);
     setColor(colorProp);
 
     if (colorProp === 'red') setIcon(red);
     if (colorProp === 'orange') setIcon(orange);
     if (colorProp === 'green') setIcon(green);
+  }
+
+  function handleShowQuestion() {
+    if (isAnswered) return;
+    setIsQuestionShowing(true);
   }
 
   return (
@@ -33,7 +40,7 @@ export function Card({ question, answer, index }) {
           toggleQuestionShowing={toggleQuestionShowing}
         />
       ) : (
-        <div className="question" onClick={setIsQuestionShowing}>
+        <div className="question" onClick={handleShowQuestion}>
           <h3 className={color}>Pergunta {index}</h3>
 
           <img src={icon} alt="" />
